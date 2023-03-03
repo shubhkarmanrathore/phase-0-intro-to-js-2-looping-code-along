270ac2f29592a2660e40da48f0a738992b0cdf56
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-03-02 23:27:00 -0800

    adds looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        const card = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        cards.push(card);
+    }
+    return cards;
+}
+
+function countDown() {
+    let count = 11;
+    while(count--) {
+        console.log(count)
+    }
+}
